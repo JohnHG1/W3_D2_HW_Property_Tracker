507188a87ea1c982be5dba5a87b796874143381e
JohnHG1 <honisz@yahoo.com>
2020-01-28 15:20:08 +0000

    second

--- models/property_tracker.rb
+++ models/property_tracker.rb
@@ -7,10 +7,80 @@ class PropertyTracker
   def initialize(options)
     @id = options["id"].to_i() if options["id"]
     @address = options['address']
-    @value = options['value']
+    @value = options['value'].to_i
     @bedrooms = options['bedrooms'].to_i
     @build = options['build']
   end
+
+
+  def save()
+    db = PG.connect({
+                dbname: "property_tracker",
+                host: "localhost"
+                })
+    sql = "INSERT INTO property_tracker
+    (address, value, bedrooms, build)
+    VALUES
+    ($1, $2, $3, $4) RETURNING id"
+    values =[@address, @value, @bedrooms, @build]
+    db.prepare("save", sql)
+    @id = db.exec_prepared("save", values).first()["id"].to_i
+    db.close()
+  end
+
+  def delete()
+    db = PG.connect({
+      dbname: "property_tracker",
+      host: "localhost"
+      })
+      sql = "DELETE FROM property_tracker WHERE id = $1"
+      values = [@id]
+      db.prepare("delete_one", sql)
+      db.exec_prepared("delete_one", values)
+      db.close
+  end
+
+  def update()
+    db = PG.connect({
+        dbname: "property_tracker",
+        host: "localhost"
+        })
+      sql = "UPDATE property_tracker
+        SET
+        (address, value, bedrooms, build) =
+        ($1, $2, $3, $4)
+        WHERE id = $5"
+        values = [@address, @value, @bedrooms, @build, @id]
+        db.prepare("update", sql)
+        db.exec_prepared("update", values)
+        db.close()
+      end
+
+
+  def self.all()
+    db = PG.connect({
+      dbname: "property_tracker",
+      host: "localhost"
+      })
+    sql = "SELECT * FROM property_tracker"
+    db.prepare("all", sql)
+    properties = db.exec_prepared("all")
+    db.close()
+    return properties.map { |order| PropertyTracker.new(properties) }
+  end
+
+  def self.delete_all()
+    db = PG.connect({
+        dbname: "property_tracker",
+        host: "localhost"
+        })
+      sql = "DELETE FROM property_tracker"
+      db.prepare("delete_all", sql)
+      db.exec_prepared("delete_all")
+      db.close()
+  end
+
+
 
 
 
