ca56f3b9fd29928c479ba5b8fd667f341321cfe7
JohnHG1 <honisz@yahoo.com>
2020-01-29 09:52:11 +0000

    Fianl edits

--- db/property_list.sql
+++ db/property_list.sql
@@ -7,3 +7,6 @@ CREATE TABLE property_tracker(
   build VARCHAR(255),
   id SERIAL PRIMARY KEY
 );
+
+
+-- # It is a good idea to start by creating the SQL file, next the CLASS file and then the console file.  It doesnt really matter what order but this was a recommended order by Zolt.
